2d2c9bfd90b414aa5278639bb4e99a93c4747acb
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-03-12 02:17:13 -0400

    [Functions] working on functions for common tasks

--- js/whatchat.js
+++ js/whatchat.js
@@ -1,14 +1,37 @@
-
-
-
-window.onload = function() {
-    function changeTitle(title){
+var App=new(function(){
+    this.changeTitle=function(title){
         h=document.getElementsByTagName('h1')[0]
         h.innerHTML=title
     }
+    this.listContacts=function(){
+        a=document.getElementsByTagName('h2')
+        r=new Array()
+        for(i=0;i<a.length;i++){
+            r.push(a[i].textContent)
+        }
+        return r
+    }
+    this.addContact=function(name,description,status){
+        c=document.getElementsByClassName('contact')[0]
+        b=c.cloneNode()
+        b.lastElementChild
+         .firstElementChild.textContent=name
+        b.lastElementChild
+         .children[1].textContent=description
+        b.lastElementChild
+         .lastElementChild
+         .setAttribute('class','status '+status)
 
+        a=document.querySelector('section')
+        d=document.querySelector('section>.clear')
+        a.insertBefore(b,d)
+        
+    }
+})()
+
+window.onload = function() {
     setTimeout(function() {
-        changeTitle('ejemplo')
+        App.changeTitle('ejemplo')
     }, 3000)
 }
 
